13a26d0e30220fbb79c7de733f5abe2a46eed862
dolatas <dodo.dolata@gmail.com>
2014-09-14 13:25:12 +0200

    node interface to node abstract
    code refactoring

--- src/main/java/pl/put/trie/Node.java
+++ src/main/java/pl/put/trie/Node.java
@@ -1,18 +1,85 @@
 package pl.put.trie;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface Node {
-	public Integer findLabel(Integer value);
-	public Node findChildByLabel(Integer label);
-	public Node findChild(Integer value);
-	public Node addChild(List<Integer> value);
-	public Node addChildByLabel(Integer label);
-	public void setLabels(List<Integer> labels);
-	public Integer getLastElement();
-	public List<Integer> getValue();
-	public void addLabel(Integer label);
-	public List<Node> getChildren();
-	public List<Integer> getLabels();
+public abstract class Node {
+
+	protected List<Integer> value;
+	protected List<Node> children;
+	protected List<Integer> labels;
+
+	public Node(List<Integer> value) {
+		this.value = value;
+		this.children = new ArrayList<Node>();
+		this.labels = new ArrayList<Integer>();
+	}
+
+	public abstract Node addChildByLabel(Integer label);
+	public abstract Node addChild(List<Integer> value);
+
 	
+	public Integer findLabel(Integer value) {
+		if (children != null) {
+			for (Integer label : labels) {
+				if (label.equals(value)) {
+					return label;
+				}
+			}
+		}
+		return null;
+	}
+
+	public Node findChildByLabel(Integer label) {
+		if (children != null) {
+			for (Node n : children) {
+				if (n.getLastElement().equals(label)) {
+					return n;
+				}
+			}
+		}
+		return null;
+
+	}
+
+	public Node findChild(Integer value) {
+		if (children != null) {
+			for (Node n : children) {
+				if (n.getValue().equals(value)) {
+					return n;
+				}
+			}
+		}
+		return null;
+	}
+
+	public List<Integer> getValue() {
+		return value;
+	}
+
+	public List<Node> getChildren() {
+		return children;
+	}
+
+	public List<Integer> getLabels() {
+		return labels;
+	}
+
+	public void addLabel(Integer label) {
+		this.labels.add(label);
+	}
+
+	public void setLabels(List<Integer> labels) {
+		this.labels = labels;
+	}
+
+	public Integer getLastElement() {
+		return value.get(value.size() - 1);
+	}
+
+	@Override
+	public String toString() {
+		return value.toString();
+	}
+
 }
--- src/main/java/pl/put/trie/NodeCCT.java
+++ src/main/java/pl/put/trie/NodeCCT.java
@@ -1,13 +1,17 @@
 package pl.put.trie;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class NodeCCT  extends NodeCC implements Node {
+public class NodeCCT extends Node {
 
 	private int[] counters;
-	public NodeCCT(List<Integer> value, int dmqSize) {
+	private int dmqNo;
+	
+	public NodeCCT(List<Integer> value, int dmqNo) {
 		super(value);
-		this.counters = new int[dmqSize];
+		this.dmqNo = dmqNo;
+		this.counters = new int[dmqNo];
 	}
 	public int[] getCounters() {
 		return counters;
@@ -17,6 +21,23 @@ public class NodeCCT  extends NodeCC implements Node {
 	}
 	public int getCounterAtIndex(int index){
 		return counters[index];
+	}
+
+	@Override
+	public Node addChild(List<Integer> value) {
+		Node n = new NodeCCT(value, dmqNo);
+		children.add(n);
+		return n;
+	}
+	
+	@Override
+	public Node addChildByLabel(Integer label) {
+		List<Integer> value = new ArrayList<Integer>();
+		value.addAll(this.value);
+		value.add(label);
+		Node n = new NodeCCT(value, dmqNo);
+		children.add(n);
+		return n;
 	}
 	
  
